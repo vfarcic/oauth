5508944e3057c0f9b816dddc5c7b616ef28d55ec
vfarcic <viktor@farcic.com>
2015-06-12 01:32:55 +0200

    Added providers

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 mongod &
-sed -i -e "s/localhost:8080/$DOMAIN:$PORT/g" /app/components/oauth/who-am-i.html
+sed -i -e "s/localhost:8080/$DOMAIN:$PORT/g" /app/components/oauth/oauth-who-am-i.html
+sed -i -e "s/localhost:8080/$DOMAIN:$PORT/g" /app/components/oauth/oauth-providers.html
 $PWD/oauth "$@"
